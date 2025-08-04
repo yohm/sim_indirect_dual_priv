86bdb13bf04fdcac54949bb72b65ce627493d4f4
Yohsuke Murase <yohsuke.murase@gmail.com>
2025-08-04 14:43:57 +0900

    remove obsolete files

--- fugaku_build_calc_fixation_probs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-# Fujitsu compiler with and without clang
-mpiFCCpx -std=c++17 -stdlib=libc++ -Nclang -Kfast -Kopenmp -I$HOME/data/sandbox/json/include -I$HOME/data/sandbox/eigen-3.3.7 -Iicecream-cpp -o main_calc_fixation_probs.out main_calc_fixation_probs.cpp
-
