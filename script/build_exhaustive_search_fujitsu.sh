5a706e836c6cbd29e084b140906f0a841ecefb60
Yohsuke Murase <yohsuke.murase@gmail.com>
2026-05-26 15:21:32 +0900

    add script to build on Fugaku

---
+++ script/build_exhaustive_search_fujitsu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
+
+CXX="${CXX:-mpiFCCpx}"
+JSON_INCLUDE="${JSON_INCLUDE:-${HOME}/data/sandbox/json/include}"
+EIGEN_INCLUDE="${EIGEN_INCLUDE:-${HOME}/data/sandbox/eigen-3.3.7}"
+OUT="${OUT:-main_ExhaustiveSearch.out}"
+
+cd "${REPO_ROOT}"
+
+"${CXX}" \
+  -std=c++17 \
+  -stdlib=libc++ \
+  -Nclang \
+  -Kfast \
+  -Kopenmp \
+  -I"${JSON_INCLUDE}" \
+  -I"${EIGEN_INCLUDE}" \
+  -Iicecream-cpp \
+  ${EXTRA_CXXFLAGS:-} \
+  -o "${OUT}" \
+  main_ExhaustiveSearch.cpp
